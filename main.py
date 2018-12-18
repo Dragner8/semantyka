2b35ff8d4c22b1fa751a0754def075d47b6bfa44
Piotr <piotrgrap@gmail.com>
2018-12-18 18:05:51 +0100

    added command with values

--- main.py
+++ main.py
@@ -9,14 +9,12 @@ def search(target ,input,json):
     return False,0,0
 
 
-def levenstain():
 
-    pass
 
 def multiple_search(x, input):
 
     counter = len(x);
-
+    tmp=[]
     for word in x:
         if word in input:
             counter -= 1
@@ -42,9 +40,19 @@ def recognize_command(input):
             result += str(Id)
             actionFound, Id, Json = search('actions', input, Json)
             if actionFound:
-                #result += Id
-                print(Id+" "+result)
-                print (Json["value"])
+
+                if(Json["value"]):
+
+                    for value in Json["value"]:
+                        if value in input:
+                            result=Id+" "+result+" "+value
+                            return result
+                    print("error set value is unavailable")
+                    return None
+                else:
+                    result=Id+" "+result
+                    return result
+
 
             else:
                 print("error unknown action")
@@ -57,14 +65,41 @@ def recognize_command(input):
         return None
 
     return result
+
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[
+                             j + 1] + 1  # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1  # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
 def main():
+    a = "wlacz"
+    b = "wylacz"
+
+    print(levenshtein(a, b))
 
 
     while(True):
         x = input('What do you want for me?(write "end" to break)')
         if x=="end":
             break
-        recognize_command(x.split())
+        print(recognize_command(x.split()))
+
 
 
 
